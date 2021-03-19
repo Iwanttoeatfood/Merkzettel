a56e3e29ebc6eea6866e0b0f00ff055cc0b5c3db
tischoerger <timm.schoerger@ead-systeme.de>
2021-03-19 13:33:49 +0100

    First commit

---
+++ src/merkzettel.java
@@ -0,0 +1,11 @@
+import com.sun.xml.internal.fastinfoset.util.StringArray;
+
+public class merkzettel {
+    String[] merk = new String[] {"","","","","",""};
+
+}
+
+public class neuenotiz(wert, String)
+{
+    String[wert] = ;
+}
